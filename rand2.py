c6830663dee13c85eb7eabec3a8b7fa9600fd06a
almohress <alimohress@gmail.com>
2021-04-14 23:39:46 +0430

    do style-formatting

--- rand2.py
+++ rand2.py
@@ -1,8 +1,8 @@
 import random
 
-def rand2(a:int,b:int):
-    random_num =1
-    while random_num%2!=0:
-        random_num=random.randint(a,b+1)
-    return random_num
 
+def rand2(a: int, b: int):
+    random_num = 1
+    while random_num % 2 != 0:
+        random_num = random.randint(a, b+1)
+    return random_num
--- swap.py
+++ swap.py
@@ -1,3 +1,3 @@
-def swap(a,b):
-  (a,b)=(b,a)
-  return (a,b)
+def swap(a, b):
+    (a, b) = (b, a)
+    return (a, b)
